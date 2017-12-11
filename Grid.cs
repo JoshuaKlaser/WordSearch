343a01ce0be6ccd3bc580a3c57e523f0a197f5c6
JoshuaKlaser <joshua_klaser@hotmail.com>
2017-12-11 14:04:29 +1100

    Initial Commit.

---
+++ Grid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random
+{
+    public class Grid
+    {
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public Grid(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+    }
+}
---
+++ GridTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random
+{
+    public class GridTile
+    {
+        public string Value { get; private set; }
+
+        public GridTile(string value)
+        {
+            Value = value;
+        }
+    }
+}
